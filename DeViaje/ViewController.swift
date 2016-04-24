509a1da00538a4340d81bc1d595e3f313772ecb8
Gerardo Guerra Pulido <gerardogup@gmail.com>
2016-04-24 09:12:16 -0500

    Initial Commit

---
+++ DeViaje/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeViaje
+//
+//  Created by Gerardo Guerra Pulido on 24/04/16.
+//  Copyright © 2016 Gerardo Guerra Pulido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
